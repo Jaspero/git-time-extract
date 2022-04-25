8a980ac4ca5b1ddc4f6c428648240eb819528fde
flauc <filip.lauc93@gmail.com>
2022-04-25 23:44:56 +0200

    chore: added time extract script

---
+++ time.js
@@ -0,0 +1,28 @@
+const { exec } = require('child_process');
+
+const [time] = process.argv.slice(2);
+const timeMap = {
+  s: 1 / 3600,
+  sec: 1 / 3600,
+  m: 1 / 60,
+  min: 1 / 60,
+  h: 1,
+  hour: 1,
+};
+
+exec(
+  `git log ${time ? `--since="${time}"` : ''} --grep="time"`,
+  (error, stdout, stderr) => {
+    const lines = stdout
+      .split('\n')
+      .filter((it) => it.startsWith('    ') && it.includes('time:'))
+      .map((it) => it.match(/time: ?(\w*)/)[1]);
+
+    const sum = lines.reduce((acc, it) => {
+      const time = timeMap[it.replace(/[0-9]*/, '')];
+      return acc + parseFloat(it, 10) * time;
+    }, 0);
+
+    console.log(sum.toFixed(2) + 'h');
+  }
+);
